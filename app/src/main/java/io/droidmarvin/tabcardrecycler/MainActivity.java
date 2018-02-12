ffa4ab6fa69c3645859653a3e909cc224201ed95
ngesa marvin <marvinngesa@gmail.com>
2018-02-13 01:07:07 +0300

    modify main layout

--- app/src/main/java/io/droidmarvin/tabcardrecycler/MainActivity.java
+++ app/src/main/java/io/droidmarvin/tabcardrecycler/MainActivity.java
@@ -1,7 +1,19 @@
 package io.droidmarvin.tabcardrecycler;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
