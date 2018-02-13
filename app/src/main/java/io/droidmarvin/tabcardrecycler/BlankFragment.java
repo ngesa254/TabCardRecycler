27127851ccf57f96744ca26a0c8522137f8418c0
ngesa marvin <marvinngesa@gmail.com>
2018-02-13 07:21:34 +0300

    add images vauxhall

--- app/src/main/java/io/droidmarvin/tabcardrecycler/BlankFragment.java
+++ app/src/main/java/io/droidmarvin/tabcardrecycler/BlankFragment.java
@@ -28,7 +28,11 @@ public class BlankFragment extends Fragment {
 
         RecyclerView rv = rootView.findViewById(R.id.rv_recycler_view);
         rv.setHasFixedSize(true);
-        MyAdapter adapter = new MyAdapter(new String[]{"test one", "test two", "test three", "test four", "test five" , "test six" , "test seven"});
+        MyAdapter adapter = new MyAdapter(new String[]{"test one",
+                "test two", "test three",
+                "test four", "test five" ,
+                "test six" , "test seven",
+                "test eight" , "test nine"});
         rv.setAdapter(adapter);
 
         LinearLayoutManager llm = new LinearLayoutManager(getActivity());
